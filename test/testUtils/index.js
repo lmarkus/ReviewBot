34c75c64b580f8daadbc1d5836b0d237f68aae21
lmarkus <lmarkus@paypal.com>
2017-02-11 20:14:06 -0800

    Adding TravisCI configuration

--- test/testUtils/index.js
+++ test/testUtils/index.js
@@ -1,6 +1,13 @@
 /**
  * Created by lmarkus on 2/8/17.
  */
+const fs = require('fs');
+
+let path = `${__dirname}/../scratch`;
+if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+}
+
 module.exports = {
 
     LowMocker: class LowMocker {
